16abbd80968cc0a4efb887686747a3423824d78d
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-09 07:14:03 +0600

    added the user constants

---
+++ backend/src/modules/user/user.constant.js
@@ -0,0 +1,21 @@
+const USER_ROLE = {
+    USER: "USER",
+    ADMIN: "ADMIN",
+};
+
+const USER_PLAN = {
+    FREE: "FREE",
+    PREMIUM: "PREMIUM",
+};
+
+const USER_STATUS = {
+    ACTIVE: "ACTIVE",
+    SUSPENDED: "SUSPENDED",
+    DELETED: "DELETED",
+};
+
+module.exports = {
+    USER_ROLE,
+    USER_PLAN,
+    USER_STATUS,
+};
