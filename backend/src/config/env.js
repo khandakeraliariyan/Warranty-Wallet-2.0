a6af5cb926643d5316bca88bc3153e442498ec90
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-09 06:27:14 +0600

    added the dot env config file

---
+++ backend/src/config/env.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    CLIENT_URL: process.env.CLIENT_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_EMAIL: process.env.SMTP_EMAIL,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+};
