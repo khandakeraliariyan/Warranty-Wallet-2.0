679fbbecb204955da37f1d1d27054f4a428c14dd
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-11 02:24:47 +0600

    updated the router with new routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
-const userRoutes = require("../modules/user/user.route");
-const categoryRoutes = require("../modules/category/category.route");
-
 const router = express.Router();
 
+const userRoutes = require("../modules/user/user.route");
+const categoryRoutes = require("../modules/category/category.route");
+const productRoutes = require("../modules/product/product.route");
+
 router.use("/users", userRoutes);
+
 router.use("/categories", categoryRoutes);
+
+router.use("/products", productRoutes);
 
 module.exports = router;
