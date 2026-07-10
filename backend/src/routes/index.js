1637f6ad159bf007e7cae72367e455f7c76921d9
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-10 13:23:45 +0600

    added the category routes and connected with the router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const userRoutes = require("../modules/user/user.route");
+const categoryRoutes = require("../modules/category/category.route");
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
