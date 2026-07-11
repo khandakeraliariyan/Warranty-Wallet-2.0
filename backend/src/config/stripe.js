49b8b0eb6fe3f5f54729f99fe427b8c804247af3
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-11 06:06:36 +0600

    solve the bugs

--- backend/src/config/stripe.js
+++ backend/src/config/stripe.js
@@ -2,6 +2,10 @@ const Stripe = require("stripe");
 
 const env = require("./env");
 
-module.exports = new Stripe(
-    env.STRIPE_SECRET_KEY
-);
+if (!env.STRIPE_SECRET_KEY) {
+  throw new Error(
+    "Missing STRIPE_SECRET_KEY in environment. Set STRIPE_SECRET_KEY in .env or your system environment."
+  );
+}
+
+module.exports = new Stripe(env.STRIPE_SECRET_KEY);
